a3322849ab7b146a12e0ded4b26e1d2d771ddf5f
Nizamudheen <nisunisam2@gmail.com>
2025-03-13 20:27:50 +0530

    env file added

--- movie-store/src/assets/services/api.js
+++ movie-store/src/assets/services/api.js
@@ -1,7 +1,7 @@
 
 
-const API_KEY = "47d53155114bb22c612ca2555730d894";
-const BASE_URL="https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_API_KEY
+const BASE_URL= import.meta.env.VITE_BASE_URL
 
 export const getPooularMovies= async()=>{
     const response= await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&with_original_language=ml&sort_by=popularity.desc`);
